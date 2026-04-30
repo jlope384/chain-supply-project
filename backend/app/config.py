dbe2d470e8a2e2a960edb05e5e03178fba22708f
PeDro0210 <pedroruben021@gmail.com>
2026-04-30 16:03:06 -0600

    fix: pydantic settings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 class Settings(BaseSettings):
-    NEO4J_URI: str = "neo4j+s://xxxxxxxx.databases.neo4j.io"
-    NEO4J_USER: str = "neo4j"
-    NEO4J_PASSWORD: str = "your-password"
-    NEO4J_DATABASE: str = "neo4j"
+    NEO4J_URI: str
+    NEO4J_USER: str = Field(alias="NEO4J_USERNAME")
+    NEO4J_PASSWORD: str
+    NEO4J_DATABASE: str
 
     class Config:
         env_file = ".env"
+        populate_by_name = True
 
 settings = Settings()
